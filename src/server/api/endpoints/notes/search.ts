07f8711872031d219a46d76284687d6a094bfcbc
CookieRamen <ark@arkjp.net>
2021-06-21 13:21:53 +0900

    ホスト名を含んだ検索に対応

--- src/server/api/endpoints/notes/search.ts
+++ src/server/api/endpoints/notes/search.ts
@@ -67,6 +67,7 @@ export default define(meta, async (ps, me) => {
 		const query = makePaginationQuery(Notes.createQueryBuilder('note'), ps.sinceId, ps.untilId);
 		const sinceRegex = /since:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])) /g;
 		const untilRegex = /until:([0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])) /g;
+		const hostRegex = /host:([a-zA-Z0-9.-]+) /g;
 
 		if (ps.userId) {
 			query.andWhere('note.userId = :userId', { userId: ps.userId });
@@ -82,6 +83,16 @@ export default define(meta, async (ps, me) => {
 			query.andWhere('note.createdAt < :until', {until: `${RegExp.$1} 23:59:59`});
 			ps.query = ps.query.replaceAll(untilRegex, '');
 		}
+		if (hostRegex.test(ps.query)) {
+			if (RegExp.$1 === 'local') {
+				query.andWhere('note.userHost IS NULL');
+			} else {
+				query.andWhere('note.userHost = :host', {host: `${RegExp.$1}`});
+			}
+			ps.query = ps.query.replaceAll(hostRegex, '');
+		}
+
+		ps.query = ps.query.replaceAll(/\s\s+/g, ' ');
 
 		query
 			.andWhere('note.text &@~ :q', { q: `${ps.query}` })
